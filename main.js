760df951f2318b40a5ba2c4b8dd639621c31870c
parth-kp <parthkp13@gmail.com>
2022-12-30 16:24:51 +0530

    adding basic car movements

--- main.js
+++ main.js
@@ -1,3 +1,18 @@
-const canvas = document.getElementById("myCanvas");
-canvas.height=window.innerHeight;
-canvas.width=250;
+const canvas = document.getElementById("myCanvas"); //Getting Canvas
+canvas.height=window.innerHeight; // Setting height to full window size to give it a feel of road
+canvas.width=450;
+
+const ctx=canvas.getContext("2d"); //Canvas.Context for puting up car in canvas
+
+const car = new Car(100,100,30,50);
+car.draw(ctx);
+
+animate();
+
+function animate(){
+    car.update();
+    canvas.height=window.innerHeight;
+    car.draw(ctx);
+    requestAnimationFrame(animate);
+}
+
